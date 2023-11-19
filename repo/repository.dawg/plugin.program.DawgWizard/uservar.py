f80a452fe3c59102f459e5792c8d978ba44a1eff
jason247g <74936035+jason247g@users.noreply.github.com>
2023-11-19 10:30:28 -0500

    Add files via upload

---
+++ repo/repository.dawg/plugin.program.DawgWizard/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/jason247g/Dawg/main/wizard_builds.json'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/jason247g/Dawg/main/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
